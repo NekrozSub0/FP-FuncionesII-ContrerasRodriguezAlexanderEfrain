34b55229fe86036bfeff3cff2c8e8023eccca9e2
Alexander <alex-c-r@hotmail.es>
2025-06-15 17:29:08 -0500

    Ejercicio 2 - generar numero aleatorio, avance 1

---
+++ ejercicio2.cpp
@@ -0,0 +1,18 @@
+//Generador de Números Aleatorios
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+	int numeromaximo, numerogenerado, ;
+	//Entrada
+	cout<<"Ingrese el numero maximo a generar aleatoriamente"
+	cin>>numeromaximo;
+	//Proceso
+	while
+	
+	//Salida
+	cout<<""
+	
+	
+}
