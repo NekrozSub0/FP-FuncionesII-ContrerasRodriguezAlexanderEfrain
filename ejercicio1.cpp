729a4c7b44fde96368b31c3ad9d05006d67be154
Alexander <alex-c-r@hotmail.es>
2025-06-15 17:18:30 -0500

    Ejercicio 1 - area de un círculo, versión final

---
+++ ejercicio1.cpp
@@ -0,0 +1,15 @@
+//Algoritmo Cálculo del Área de un Círculo
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	double areacirculo, radio;
+	//Entrada
+	cout<<"Ingrese el radio del circulo"<<endl;
+	cin>>radio;
+	//Proceso
+	areacirculo=(M_PI*(pow(radio,2)));
+	//Salida
+	cout<<"El area del ciruclo es: "<<areacirculo<<endl;
+}
