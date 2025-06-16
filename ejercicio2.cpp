88e32cd6c66f2b9491ea83fbe9a26e8ec77f548b
Alexander <alex-c-r@hotmail.es>
2025-06-15 20:53:56 -0500

    Ejercicio2 - numeros aleatorios, version final

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,18 +1,21 @@
 //Generador de Números Aleatorios
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
-	int numeromaximo, numerogenerado, ;
+	int numeromaximo, numeroaleatorio, numerosgenerados;
 	//Entrada
-	cout<<"Ingrese el numero maximo a generar aleatoriamente"
-	cin>>numeromaximo;
-	//Proceso
-	while
-	
-	//Salida
-	cout<<""
-	
-	
+	cout<<"Ingrese el numero maximo para generar numeros aleatoriamente"<<endl;
+	cin>>numeromaximo;	
+	cout<<"Ingrese la cantidad de numeros generados"<<endl;
+	cin>>numerosgenerados;
+	//Proceso y Salida
+	srand(time(NULL));
+	for(int i=1;i<=numerosgenerados;i++){
+	numeroaleatorio=rand()%(numeromaximo+1);
+	cout<<"Numero aleatorio generado: "<<numeroaleatorio<<endl;
+	}
+	return 0;
 }
